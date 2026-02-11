1b75b812ba2aee0f7c72ec6b166910aabd12922f
Hearsch-devops <hearschdevops@gmail.com>
2026-02-11 13:40:47 +0530

    Update resource.tf

--- Day4/resource.tf
+++ Day4/resource.tf
@@ -79,3 +79,12 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
+resource "aws_eip" "lb-main" {
+  instance = aws_instance.aws-main.id
+  depends_on = [ aws_internet_gateway.gw-main ]
+
+  tags = {
+    Name = "lb-main"
+  }
+
+}
